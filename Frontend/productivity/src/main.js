39ae520cb5784bfb6dac9237c09f0e61c6d4a3e6
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-12-10 18:03:30 +0100

    Définition du fichier main.js dans le dossier productivity/src avec import des diférent modules nécéssaires pour le routing

--- Frontend/productivity/src/main.js
+++ Frontend/productivity/src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
+//import { createApp } from 'vue'
+import Vue, { h } from 'vue'
 import App from './App.vue'
+import router from './routes'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+// import bootstrap from 'bootstrap/dist/css/'
+import VueRouter from 'vue-router'
 
-createApp(App).mount('#app')
+
+Vue.use(VueAxios, axios)
+
+Vue.use(VueRouter)
+
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
+
+//createApp(App).mount('#app')
