2eac46063b43bd11bf0b76d8afa1e680d46ead84
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-12-30 22:37:56 +0100

    Import des models dans app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const db = require('./database/db.script');
+const User = require('./models/user');
+const Post = require('./models/post');
+const Comment = require('./models/comment');
 
 app.use(cors());
 
