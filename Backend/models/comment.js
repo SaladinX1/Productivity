f0c46846ecf23bb5adc23b7ce655f89a73229769
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-01-06 00:20:14 +0100

    Établissement de la table comment et post

--- Backend/models/comment.js
+++ Backend/models/comment.js
@@ -4,7 +4,9 @@ const db = require('../database/db.script');
 
 const Comment = db.query(`CREATE TABLE IF NOT EXISTS Comment
                          (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                             message VARCHAR(255) )`, 
+                             message VARCHAR(255),
+                             user_id INT NOT NULL,
+                            FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE)`, 
                              
     function( err , result) {
     if (err) throw err;
--- Backend/models/post.js
+++ Backend/models/post.js
@@ -1,4 +1,6 @@
 const db = require('../database/db.script');
+const User = require('./user');
+
 
 
 
@@ -6,7 +8,11 @@ const Post = db.query(`CREATE TABLE IF NOT EXISTS Post
                                 (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                                      title VARCHAR(255) NOT NULL,
                                       picture VARCHAR(255),
-                                       message VARCHAR(255))`, 
+                                       message VARCHAR(255),
+                                        user_id INT NOT NULL,
+                                       FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
+                                      )`, 
+
 function( err , result) {
     if (err) throw err;
     console.log("Table Post crée");
