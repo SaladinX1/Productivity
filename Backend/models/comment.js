db74acdc474d5b9acb84221f0a2d79be4c4e7896
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-01-10 12:17:21 +0100

    Changement de foreign key dans la table commen.js pour post_id

--- Backend/models/comment.js
+++ Backend/models/comment.js
@@ -5,8 +5,8 @@ const db = require('../database/db.script');
 const Comment = db.query(`CREATE TABLE IF NOT EXISTS Comment
                          (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                              message VARCHAR(255),
-                             user_id INT NOT NULL,
-                            FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE)`, 
+                             post_id INT NOT NULL,
+                            FOREIGN KEY (user_id) REFERENCES Post(id) ON DELETE CASCADE)`, 
                              
     function( err , result) {
     if (err) throw err;
