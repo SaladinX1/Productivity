10a22b98eaf2121b2b4d0a484ae286077f025002
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-01-17 23:52:57 +0100

    Établissement des controllers putPost, deletePost et getOnePost

--- Backend/controllers/post.js
+++ Backend/controllers/post.js
@@ -6,11 +6,11 @@ exports.allPosts = (req,res, next) => {
 
   const Post = `SELECT * FROM 'Post' `
 
-    db.query(Post,(result, err) => {
+    db.query(Post,(err, result) => {
         if(!result) {
             res.status(400).json({message:'Echec de la recuperation, mauvaise requête !'})
         } else {
-            res.status(200).json({message: 'Posts récupérés !'})
+            res.status(200).json(result);
         }
     })
 
@@ -20,6 +20,51 @@ exports.onePost = (req, res,next) => {
 
     const id = req.params.id;
 
+    const getOnePost = `SELECT FROM Post WHERE 'id' = ${id} `;
+
+    db.query(getOnePost,(err, result) => {
+        if(!result) {
+            res.status(400).json({message: 'Mauvaise requête'})
+        } else {
+            res.status(200).json({message: 'Post récupéré'})
+        }
+    })
+}
+
+
+exports.deletePost = (req,res, next) => {
+
+    const id = req.params.id;
+
+    const deletePost = `DELETE FROM Post WHERE 'id' = ${id}`;
     
+    
+    db.query(deletePost, (err, result) => {
+        if(!result) {
+            res.status(400).json({message:'Mauvaise requête'});
+        } else {
+            res.status(200).json({message: 'Post Suprimé !'})
+        }
+    });
+}
+
+exports.putPost = (req, res, next) => {
+
+    const id = req.params.id;
+
+    const {
+        message, picture
+    } = req.body;
+
+    const putPost = `UPDATE Post SET '${message}', '${picture}' WHERE 'id' = ${id}; `
+
+
+    db.query(putPost, (err, result) => {
+        if(!result) {
+            res.status(400).json({message: 'Mauvaise requête'});
+        } else {
+            res.status(200).json({message: 'Post Modifié !'})
+        }
+    });
 
 }
