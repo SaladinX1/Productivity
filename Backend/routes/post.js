074383435ff5a600c33f97a8da4be9dbb33c780e
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-01-18 21:41:29 +0100

    Établissement des routes post

--- Backend/routes/post.js
+++ Backend/routes/post.js
@@ -3,5 +3,9 @@ const router = express.Router();
 
 const postCtrl = require('../controllers/post');
 
-router.get('/allPosts', postCtrl.allPosts);
+router.get('/allposts', postCtrl.allPosts);
 router.get('/post', postCtrl.onePost);
+router.put('/putpost', postCtrl.putPost);
+router.delete('/deletepost', postCtrl.deletePost);
+
+module.exports = router;
