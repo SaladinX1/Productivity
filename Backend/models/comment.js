b9b170a04c43eca1ba3799057758eaee29d39c4f
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-01-22 23:37:11 +0100

    Établissement de la foreign key post_id pour la table comment

--- Backend/models/comment.js
+++ Backend/models/comment.js
@@ -6,7 +6,7 @@ const Comment = db.query(`CREATE TABLE IF NOT EXISTS Comment
                          (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                              message VARCHAR(255),
                              post_id INT NOT NULL,
-                            FOREIGN KEY (user_id) REFERENCES Post(id) ON DELETE CASCADE)`, 
+                            FOREIGN KEY (post_id) REFERENCES Post(id) ON DELETE CASCADE)`, 
                              
     function( err , result) {
     if (err) throw err;
