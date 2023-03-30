c307c3532b44772b00a174d7d0c5b61f0bbfbc22
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-03-31 00:24:19 +0200

    Ajout du midleware middleware multer aux routes post

--- Backend/routes/post.js
+++ Backend/routes/post.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
-
+const auth = require('../middlewares/auth');
 const postCtrl = require('../controllers/post');
 
-router.get('/allposts', postCtrl.allPosts);
-router.get('/post', postCtrl.onePost);
-router.put('/putpost', postCtrl.putPost);
-router.delete('/deletepost', postCtrl.deletePost);
+// destination du fichier
+const upload = multer({ dest: '/images' }); 
+
+//app.post('/api/addpost', auth, , postCtrl.addPost);
+
+router.get('/allposts',auth,  postCtrl.allPosts);
+router.get('/post/:id',auth, postCtrl.onePost);
+router.post('/addpost', auth, upload.single('picture'), postCtrl.addPost);
+router.put('/putpost',auth, postCtrl.putPost);
+router.delete('/deletepost',auth, postCtrl.deletePost);
 
 module.exports = router;
