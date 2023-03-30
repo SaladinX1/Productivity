70e4880ccb6ceb2282bbc6504cc8a6cdad10dcde
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-03-31 00:22:28 +0200

    Ajout du midleware auth aux routes post

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../middlewares/auth');
 const userCtrl = require('../controllers/user');
 
 router.post('/registryuser', userCtrl.register);
 router.post('/connectuser', userCtrl.login);
-router.put('/putuser', userCtrl.putUser);
-router.get('/getuser/:id', userCtrl.getUser);
-router.delete('/deleteuser/:id', userCtrl.deleteUser);
+router.put('/putuser',auth, userCtrl.putUser);
+router.get('/getuser/:id',auth, userCtrl.getUser);
+router.delete('/deleteuser/:id', auth, userCtrl.deleteUser);
 
 module.exports = router
