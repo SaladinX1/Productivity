8e25ac642b7bcaa5fd5703b770042b99cbfd2b42
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-04-02 11:42:16 +0200

    Établissement affichage post depuis bdd + ajout contrôle remplissage champs avant envoi Scan

--- Frontend/js/data.js
+++ Frontend/js/data.js
@@ -1,8 +1,10 @@
 const data_userProfile = document.querySelector('.info_user__data');
 const user_id = localStorage.getItem('user_id');
-
-
-
+const blog = document.querySelector('.fil__blog');
+const overlayPostScan = document.querySelector('.fil__overlayPost');
+const btnDisplayOverlay = document.querySelector('.fil__addPostBtn');
+const btnCancelOverlay = document.querySelector('#btnCancelPost');
+const token = localStorage.getItem('token');
 
 window.addEventListener('load', () => {
     if(document.URL.includes('profil.html')) {
@@ -60,6 +62,47 @@ function deleteAccount() {
 
 //////////////  POST ///////////////////
 
+ if (document.URL.includes('fil-posts.html')) {
+
+    fetch('http://localhost:3000/api/allposts', {
+        method: "GET",
+        headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'authorization' : `Bearer ${token}`    
+        }
+    })
+    .then(res => { return res.json()})
+    .then(data => {
+
+        console.log(data);
+
+        for(let info of data ) {
+            console.log(info.picture);
+            blog.innerHTML += `
+            <div class='fil__blog--post'>
+                    <h1>${info.title}</h1>
+                    <img src='${info.picture}' alt='Image du post'/>
+                    <div class="fil__blog--post_sampleAnnounce">
+                    <p>${info.message}</p>
+                    </div>
+                    
+            </div>
+            `
+        }
+    })
+
+    btnDisplayOverlay.addEventListener('click', () => {
+        overlayPostScan.style.display = 'block';
+    })
+
+    btnCancelOverlay.addEventListener('click', () => {
+        overlayPostScan.style.display = 'none';
+    })
+
+ }
+    
+
 
 ////////////////  AJOUT SCAN POST REQUEST //////////////////
 function sendScan() {
@@ -74,20 +117,34 @@ function sendScan() {
     data.append('picture', picture);
     data.append('message', message);
 
+    if(title != '' && !picture && message != '') {
+        fetch(`http://localhost:3000/api/addpost`, {
+            method: 'POST',
+            body: data,
+            headers : {
+                'authorization' : `Bearer ${token}`
+            } 
+           
+        })
+        .then(data => {return data.json()})
+        .then(res => {
+            console.log(res);
+    
+            alert('Scan crée ! Bien joué !')
+            location.reload();
+        })
 
-    fetch(`http://localhost:3000/api/addpost`, {
-        method: 'POST',
-        body: data,
-        headers : {
-            'authorization' : `Bearer ${token}`
-        } 
-       
-    })
-    .then(data => {return data.json()})
-    .then(res => {
-        console.log(res);
+    } else {
+        document.querySelector('.errMsgScan').style.color = 'red';
+        document.querySelector('.errMsgScan').style.fontSize = '1.3rem';
+        document.querySelector('.errMsgScan').style.fontWeight = 'bold';
+        document.querySelector('.errMsgScan').textContent = `Veuillez remplir tous les champs, merci.`;
 
-        alert('Scan crée ! Bien joué !')
-        location.reload();
-    })
-}
+        setTimeout(() => {
+            document.querySelector('.errMsgScan').textContent = ``;
+        },1800);
+    }
+
+    }
+
+   
