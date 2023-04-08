df1e16dc406918dd689b881cf33d388666ec0ef8
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-04-08 12:45:05 +0200

    Établissement des routes post et commentaires

--- Backend/app.js
+++ Backend/app.js
@@ -4,8 +4,14 @@ const path = require('path');
 const cors = require('cors');
 const db = require('./database/db.script');
 const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/post');
+const commentRoutes = require('./routes/comment');
 
 app.use(cors());
+
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 app.use(express.json());
 
@@ -13,7 +19,12 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 app.use('/api', userRoutes);
+app.use('/api', postRoutes);
+app.use('/api', commentRoutes);
+
 
 module.exports = app;
