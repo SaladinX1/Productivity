65914a2117319bd0c815428992624a38241f1a66
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-05-23 11:07:03 +0200

    Modification access-control-allow-method dans server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,13 @@ const http = require('http');
 const server = http.createServer(app);
 
 server.listen(3000, () => {
-   // console.log('Congrats , the server is listening on port 3000');
+    console.log('Congrats , the server is listening on port 3000');
 });
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'PATCH, PUT, GET, POST, DELETE, HEAD');
     next();
 });
 
