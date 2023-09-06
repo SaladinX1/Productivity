0bb5b829f2374a3a1176a630e14daac65fe13be2
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-09-06 11:41:06 +0200

    Maj package json + server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,9 @@
-#!/usr/bin/env node
 
 
 const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
-server.listen(process.env.APPPORT || 3000 , () => {
+server.listen(process.env.PORT || 8000 , () => {
     console.log('Congrats , the server is listening on port 3000');
 });
 
